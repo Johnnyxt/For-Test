b92ad3187cffdbec4c42db56a6e0b1bb46ab9809
Johnnyxt <Junlee1102@163.com>
2017-03-30 10:36:21 +0800

    加入NotificationObject

---
+++ JW18001B/NotificationObject.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel;
+
+namespace JW18001B
+{
+    public class NotificationObject : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void RaisePropertyChanged(string propertyName)
+        {
+            if (this.PropertyChanged != null)
+            {
+                this.PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
